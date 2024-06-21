4c282a069405f89864ce6b850649980a3c4871e7
javangala <javangalamahesh1501@gmail.com>
2024-06-21 20:45:51 +0530

    from Git-Demo

---
+++ src/a_Ruff/AAAAA.java
@@ -0,0 +1,26 @@
+package a_Ruff;
+
+public class AAAAA {
+	
+	public static void main(String[] args) {
+		AAAAA a=new AAAAA();
+		a.method1();
+		a.method2();
+		method_1();
+		
+	}
+	
+	public void method1() {
+		System.out.println("exicute the statement");
+	}
+    public void method2() {
+		System.out.println("exicute the statement");
+
+	}
+   public static void method_1() {
+		System.out.println("exicute the statement");
+
+	}
+    
+
+}
