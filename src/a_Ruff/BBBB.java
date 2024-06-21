75f75268cd5d42f60e74bbe63e9321b3976a977b
javangala <javangalamahesh1501@gmail.com>
2024-06-21 20:58:23 +0530

    from Samplr git

---
+++ src/a_Ruff/BBBB.java
@@ -0,0 +1,14 @@
+package a_Ruff;
+
+public class BBBB {
+	
+	public static void main(String[] args) {
+		
+		BBBB b=new BBBB();
+		b.method();
+	}
+	public void method() {
+		System.out.println("Good Night");
+	}
+
+}
