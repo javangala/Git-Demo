3921ce70170ff48d56c19227b942fc759698942d
javangala <javangalamahesh1501@gmail.com>
2024-06-22 12:53:04 +0530

    from Git-Demo

--- src/a_Ruff/Ruff_6.java
+++ src/a_Ruff/Ruff_6.java
@@ -2,31 +2,31 @@ package a_Ruff;
 
 public class Ruff_6 {
 	public static void main(String[] args) {
-		
+
 		String s="mahesh";
 		System.out.println("Say 'Good mornig' Untill 12 pm");
 		System.out.println("Say 'Good Afternoon' Untill 5 pm");
 		System.out.println("Say 'Good Evening' Untill 8 pm");
 		System.out.println("Say 'Good Night'  8 pm onwords");
-		
+
 
 		System.out.println(Uppercase(s));
 		System.out.println(Lowercase(s));
 
 	}
-	
+
 	public static String Uppercase(String s) {
-		
+
 		String temp=s.toUpperCase();
-		
+
 		return temp;
 	}
-	
+
 	public static String Lowercase(String s) {
-		
+
 		String temp=s.toLowerCase();
+
 		return temp;
-		
 	}
 
 }
